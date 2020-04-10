7a8f4bd36e97dbdc22f6fb125a3abe6f86c97d34
brian gekara <bruvusky@gmail.com>
2020-04-10 13:54:09 +0300

    removed some code

--- greetings.py
+++ greetings.py
@@ -1,22 +1,10 @@
-import math
-import os
-import sys
 
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(whotogreet):
-    greeting = "hello, {}".format(whotogreet)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-print("I love python")
+print(r.ok)
 
-name = input("your name?")
-print("Hello,", name)
 
