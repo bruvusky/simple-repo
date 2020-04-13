a61677ef740a6bbae2dfa6f53d6999113b4fd39d
brian gekara <bruvusky@gmail.com>
2020-04-13 15:24:30 +0300

    more code

--- javascript crash course/static/js/script.js
+++ javascript crash course/static/js/script.js
@@ -4,13 +4,11 @@ function yob() {
     var ageindays = ((2020 - birthyear) * 365);
     var h1 = document.createElement('h1');
     var textanswer = document.createTextNode('you are ' + ageindays + 'days old.');
-    h1.setAttribute('id', 'ageindays');
+    h1.setAttribute('id', 'age');
     h1.appendChild(textanswer);
     document.getElementById('flex-box-result').appendChild(h1);
 }
 
-function myFunction() {
-    document.getElementById("ageindays").remove;
-
+function reset() {
+    document.getElementById('age').remove;
 }
-// <button class='btn btn-danger' onclick="reset()">reset here</button>
